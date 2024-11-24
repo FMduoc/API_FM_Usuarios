023979dfd85111e7ec5506b5ad4a7a1650aee6cd
Linkrgx <johngoogloid@gmail.com>
2024-11-23 22:05:53 -0300

    Fix a error en el código

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router =jsonServer.router("jsonOFDEATH.son");
+const router =jsonServer.router("jsonOFDEATH.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 10000;
 
